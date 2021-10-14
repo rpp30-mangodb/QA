d5f6039800e77857b6fb19905f7baf13b4f7eea0
Surekha Weinberg <s.kaniget@gmail.com>
2021-10-13 20:38:23 -0700

    install server dependencies, setup server, setup linter

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.listen(PORT, function () {
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
+})
