105c3f7c7ea3f277973b0c751c9938531daea42d
Surekha Weinberg <s.kaniget@gmail.com>
2021-10-29 13:51:15 -0700

    modify models and schema to reflect shape of data necessary for FEC response with minimal queries after aggregation in CLI

--- models/questionModel.js
+++ models/questionModel.js
@@ -4,15 +4,15 @@ const answer = require('./answerModel');
 const { Schema } = mongoose;
 
 const questionSchema = new Schema({
-  'id': {type: Number, required: true},
+  'question_id': {type: Number, required: true},
   'product_id': {type: Number, required: true},
   'question_body': {type: String, required: true},
   'question_date': {type: Date, required: true},
   'asker_name': {type: String, required: true},
   'asker_email': {type: String, required: true},
-  'question_helpfulness': {type: Number, required: true},
-  'reported': {type: Boolean, default: false}, // update when it is true
-  // 'answers': {type: Schema.Types.Mixed, required: true}
+  'question_helpfulness': {type: Number, required: false, default: 0},
+  'reported': {type: Boolean, required: false, default: false}, // update when it is true
+  'answers': {type: Schema.Types.Mixed, required: true}
 });
 
 const Question = mongoose.model('Question', questionSchema, 'questions');
