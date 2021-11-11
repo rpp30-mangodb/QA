92779527996d9f7dff4f62bdb8e59ad3d586373e
Surekha Weinberg <s.kaniget@gmail.com>
2021-11-11 15:20:12 -0800

    change routes to 'api/qa' to match original FEC routes for QA

--- __tests__/apiSpec/getAnswers.js
+++ __tests__/apiSpec/getAnswers.js
@@ -14,6 +14,6 @@ jest.setTimeout(10000);
 // get answers request - frisby.get('url');
 it ('GET should return a status of 200 OK', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions/209511/answers?page=1&count=100')
+    .get('http://localhost:5000/api/qa/questions/209511/answers?page=1&count=100')
     .expect('status', 200);
 });
--- __tests__/apiSpec/getQuestions.js
+++ __tests__/apiSpec/getQuestions.js
@@ -14,42 +14,42 @@ jest.setTimeout(10000);
 
 it ('GET should return a status of 400 bad request if product_id is undefined', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=undefined&page=1&count=100')
+    .get('http://localhost:5000/api/qa/questions?product_id=undefined&page=1&count=100')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 400 bad request if page is undefined', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=59553&page=undefined&count=100')
+    .get('http://localhost:5000/api/qa/questions?product_id=59553&page=undefined&count=100')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 400 bad request if count is undefined', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=59553&page=1&count=undefined')
+    .get('http://localhost:5000/api/qa/questions?product_id=59553&page=1&count=undefined')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 400 bad request if product_id is not an integer', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=five&page=1&count=100')
+    .get('http://localhost:5000/api/qa/questions?product_id=five&page=1&count=100')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 400 bad request if page is not an integer', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=59553&page=one&count=100')
+    .get('http://localhost:5000/api/qa/questions?product_id=59553&page=one&count=100')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 400 bad request if count is not an intger', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=59553&page=1&count=hundred')
+    .get('http://localhost:5000/api/qa/questions?product_id=59553&page=1&count=hundred')
     .expect('status', 400);
 });
 
 it ('GET should return a status of 200 OK with valid parameters', function () {
   return frisby
-    .get('http://localhost:5000/qa/questions?product_id=59553&page=1&count=100')
+    .get('http://localhost:5000/api/qa/questions?product_id=59553&page=1&count=100')
     .expect('status', 200);
 });
