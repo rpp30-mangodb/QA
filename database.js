4180c115c765cad1bf78f961ba181372dcd5adee
Surekha Weinberg <s.kaniget@gmail.com>
2021-11-11 16:24:26 -0800

    fix typo

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/atelier', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://surekha:lavi@71.83.128.153/atelier', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://surekha:lavi@71.83.128.153/atelier', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
